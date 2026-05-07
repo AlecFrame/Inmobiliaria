68e08a4c9565e6b6150a3110c9e282c56c35e8d3
lucasalonia <141883464+lucasalonia@users.noreply.github.com>
2026-05-07 15:12:42 -0300

    Probando compatibilidad y configuracion de AndroidManifest

--- app/src/main/java/com/vertacnik/inmobiliaria/ui/LoginActivity.java
+++ app/src/main/java/com/vertacnik/inmobiliaria/ui/LoginActivity.java
@@ -16,5 +16,14 @@ public class LoginActivity extends AppCompatActivity {
         b = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
 
+
+
+        /**Actualmente login funciona como la activity
+         *
+         * con el que se inicia la navegacion.
+         * Quizas entro momento se deba crear un metodo en el
+         * viewmodel que verifique la existencia de un token si es el
+         * caso esta activiy navega directamente hacia otra vista */
+
     }
 }
